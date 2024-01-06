a5a00dd5088616253e5af1273e746f66cdf33507
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2024-01-06 23:53:17 +0000

    setup backend

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,8 +2,9 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 from api.models import db, User
-from api.utils import generate_sitemap, APIException
+from api.utils import generate_sitemap, APIException, get_hash
 from flask_cors import CORS
 
 api = Blueprint('api', __name__)
@@ -20,3 +21,25 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+@api.route("/any-route", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+@api.route("/get-hash", methods=["POST"])
+def handle_get_hash():
+    to_hash = request.json.get("string")
+    return get_hash(to_hash)
