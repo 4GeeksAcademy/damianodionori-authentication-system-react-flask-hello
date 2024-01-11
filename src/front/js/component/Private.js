b0393aa75dcba333897214964e0f5d0f0d19d55e
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2024-01-11 10:38:18 +0000

    useEffect added to the private component

--- src/front/js/component/Private.js
+++ src/front/js/component/Private.js
@@ -1,10 +1,40 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import '../../styles/authForms.css';
 
 const Private = () => {
-  const [message, setMessage] = useState('Example');
-
-  useEffect( () => {}, [])
+    const [message, setMessage] = useState('Example');
+    const history = useHistory();
+  
+    useEffect(() => {
+        const fetchPrivateData = async () => {
+          try {
+            const token = sessionStorage.getItem('jwt-token');
+    
+            const resp = await fetch(`/api/private`, {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token,
+              },
+            });
+    
+            if (!resp.ok) {
+              throw new Error('There was a problem in the private request');
+            }
+    
+            const data = await resp.json();
+            console.log('This is the data you requested', data);
+            setMessage(data.message); // Assuming your response has a "message" property
+          } catch (error) {
+            console.error(error.message);
+    
+            history.push('/login');
+          }
+        };
+    
+        fetchPrivateData();
+      }, [history]);
 
   return (
     <div>
